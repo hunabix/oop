1bc665ae55d1b63a3c0f3b6cbf58d34d0eeff92d
Alejandro Sobrevilla <hunabix@gmail.com>
2017-07-03 10:37:13 -0500

    Agrego mis archivos base

---
+++ index.php
@@ -0,0 +1,10 @@
+
+
+<?php include('inc/header.php'); ?>
+
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+<?php include('inc/footer.php'); ?>
+        
+        
